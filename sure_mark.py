f922f02160be2233560d5860c6e0f6ebeddb17ff
Ostheer <mathijs@verhaegh.nl>
2020-02-08 22:02:41 +0100

    supersimple example

---
+++ sure_mark.py
@@ -0,0 +1,28 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
+
+updater = Updater(token="895077791:AAGVXZvjzPjulYwpJ7pyeBv1LOWcDWByf10", use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+
+
+updater.start_polling()
