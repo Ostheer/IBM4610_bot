6bdbff8cd7d76c3090e5dc2e969f726acf313d9f
Ostheer <mathijs@verhaegh.nl>
2020-02-15 19:32:21 +0100

    wildcard for print queue

--- print_poller/print_contents.bat
+++ print_poller/print_contents.bat
@@ -1,8 +1,10 @@
+
 :loop
 
-if exist "z:\telegram_bot\to_print.txt" (
+if exist "z:\telegram_bot\to_print*" (
 "C:\Program Files\Microsoft Office\OFFICE11\WINWORD.EXE" empty_receipt.doc /mappendPrintCloseDelete
 )
+
 
 ping 192.0.2.1 -n 1 -w 500 >nul
 
