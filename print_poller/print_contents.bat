1b80c00bfff769df8180a9e1f540301cc54f025d
Ostheer <mathijs@verhaegh.nl>
2020-03-30 14:18:03 +0200

    add docx support

--- print_poller/print_contents.bat
+++ print_poller/print_contents.bat
@@ -2,11 +2,15 @@
 :loop
 
 for %%f in (z:\telegram_bot\to_print*.doc) do (
-"C:\Program Files\Microsoft Office\OFFICE11\WINWORD.EXE" "z:\telegram_bot\%%~nf.doc" /mPrintCloseDelete
+"C:\Program Files\Microsoft Office\OFFICE12\WINWORD.EXE" "z:\telegram_bot\%%~nf.doc" /mPrintCloseDelete
+)
+
+for %%f in (z:\telegram_bot\to_print*.docx) do (
+"C:\Program Files\Microsoft Office\OFFICE12\WINWORD.EXE" "z:\telegram_bot\%%~nf.docx" /mPrintCloseDelete
 )
 
 if exist "z:\telegram_bot\to_print*" (
-"C:\Program Files\Microsoft Office\OFFICE11\WINWORD.EXE" empty_receipt.doc /mappendPrintCloseDelete
+"C:\Program Files\Microsoft Office\OFFICE12\WINWORD.EXE" empty_receipt.doc /mappendPrintCloseDelete
 )
 
 CSCRIPT SLEEP.VBS 500
